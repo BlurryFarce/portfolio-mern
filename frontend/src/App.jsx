b5c7b4835fb88396433bab59ce3856d38edad40a
Vaibhav Baria <kiboyagami0@gmail.com>
2024-08-17 15:10:43 -0400

    fixing css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
-import React from "react";
-import "./App.module.scss";
+import styles from "./App.module.scss";
 import BaseLayout from "./components/BaseLayout";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <div className={styles.app}>
       <BrowserRouter>
         <BaseLayout />
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
